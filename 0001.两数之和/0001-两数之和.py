44ce2647c9e3d235be4b2d422798cb45127d799c
kingcheng2000 <87102076@qq.com>
2020-12-17 11:10:31 +0800

    Update 0001-两数之和.py

--- 0001.两数之和/0001-两数之和.py
+++ 0001.两数之和/0001-两数之和.py
@@ -10,3 +10,21 @@ class Solution(object):
             if target - num in dic:
                 return [dic[target - num], i]
             dic[num] = i 
+
+class Solution2 (object):
+    '''
+    Brute Force 
+    The worst solution
+    Time Complexity : O(n^2)
+    Space Complexity : O(1)
+    '''
+    # def twoSum(self,nums1:list, target1:int) -> list:
+    def twoSum(self,nums1:list, target1:int):
+        for i in range(len(nums1)):
+            for j in range( i+1, len(nums1)) :
+                if nums1[i] + nums1[j] == target1:
+                    return [i,j]
+
+
+a2 = Solution2()
+b2 = a2.twoSum([1,2,3,4,5,6,7,8,9], 16)
